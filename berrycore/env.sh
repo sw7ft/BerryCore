05bee588cc0550d7bb79a1c06ece89679aea7456
sw7ft <149521204+sw7ft@users.noreply.github.com>
2025-10-26 00:05:07 -0700

    reorg of files

--- berrycore/env.sh
+++ berrycore/env.sh
@@ -56,7 +56,7 @@ export LC_CTYPE=en_US.UTF-8
 # Welcome Message
 echo ""
 echo "==========================================================="
-echo "     BerryCore v0.6 - QNX Extended Userland"
+echo "     BerryCore v0.71 - QNX Extended Userland"
 echo "==========================================================="
 echo ""
 echo "69 packages installed with 100+ binaries available"
